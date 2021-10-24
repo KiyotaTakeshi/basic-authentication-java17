a2a1a0fafcb5fe60f5b0ae5f7ff7931001d01be3
Kiyota iMac <kiyotatakeshi0609@gmail.com>
2021-10-24 13:40:06 +0900

    introduce testcontainers

--- src/test/java/com/kiyotakeshi/basicauthentication/SessionTest.java
+++ src/test/java/com/kiyotakeshi/basicauthentication/SessionTest.java
@@ -1,6 +1,5 @@
 package com.kiyotakeshi.basicauthentication;
 
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -13,8 +12,13 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 import redis.clients.jedis.Jedis;
-import redis.embedded.RedisServer;
 
 import java.io.IOException;
 import java.util.Base64;
@@ -22,18 +26,15 @@ import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-// @refer https://github.com/eugenp/tutorials/blob/master/spring-security-modules/spring-session/spring-session-redis/src/test/java/com/baeldung/spring/session/SessionControllerIntegrationTest.java
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Import(RedisTestConfiguration.class)
+@Testcontainers
 public class SessionTest {
 
-    // TODO: Testcontainers 使う
-    private static RedisServer redisServer;
     private static TestRestTemplate testRestTemplate;
     private static TestRestTemplate testRestTemplateWithAuth;
-    private Jedis jedis;
 
-    public static final Integer REDIS_PORT = 6379;
+    private Jedis jedis;
 
     private final String EXPECTED = """
                 [{"id":1,"name":"mike","department":"sales"},{"id":2,"name":"popcorn","department":"human resources"}]""";
@@ -51,22 +52,30 @@ public class SessionTest {
         return "http://localhost:" + port + "/api/employees";
     }
 
+    // 08:51:44.954 [main] DEBUG 🐳 [redis:6.2.6-alpine] - Starting container: redis:6.2.6-alpine
+    // 08:51:44.954 [main] INFO 🐳 [redis:6.2.6-alpine] - Creating container for image: redis:6.2.6-alpine
+    // 08:51:44.989 [main] INFO 🐳 [redis:6.2.6-alpine] - Starting container with ID: 110caefe02be8e2b70e54447126d32e550ba9d9306a4c27032edd8bb6bac2382
+    // 08:51:45.205 [main] INFO 🐳 [redis:6.2.6-alpine] - Container redis:6.2.6-alpine is starting: 110caefe02be8e2b70e54447126d32e550ba9d9306a4c27032edd8bb6bac2382
+    @Container
+    private static final GenericContainer REDIS = new GenericContainer(DockerImageName.parse("redis:6.2.6-alpine")).withExposedPorts(6379);
+
+    @DynamicPropertySource
+    static void redisProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.redis.port", () -> REDIS.getMappedPort(6379));
+    }
+
     @BeforeAll
     static void beforeAll() throws IOException {
         testRestTemplate = new TestRestTemplate();
         testRestTemplateWithAuth = new TestRestTemplate("user", "9c025395-5d88-4610-bef5-bb6949758e0c", null);
-        redisServer = new RedisServer(REDIS_PORT);
-        redisServer.start();
     }
 
-    @AfterAll
-    static void afterAll() {
-        redisServer.stop();
-    }
 
     @BeforeEach
     void clearRedisData() {
-        jedis = new Jedis("localhost", REDIS_PORT);
+        String address = REDIS.getHost();
+        Integer port = REDIS.getFirstMappedPort();
+        jedis = new Jedis(address, port);
         jedis.flushAll();
     }
 
